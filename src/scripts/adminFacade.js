59e06964a41307c9f199456c463a2183dd55250e
claus0200 <claus.peter.joergensen@gmail.com>
2025-01-08 13:10:42 +0100

    update

--- src/scripts/adminFacade.js
+++ src/scripts/adminFacade.js
@@ -6,21 +6,36 @@ function adminFunctions() {
 
     const fetchLentbooks = (setLentBooks) => {
         fetch(URL + "/lendbooks", facade.makeOptions("GET", true))
-            .then((response) => response.json())
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                return response.json();
+            })
             .then((data) => setLentBooks(data))
             .catch((error) => console.log("An error occurred: ", error))
     }
     
     const editLentbook = (id) => {
         fetch(URL + "/lendbooks/" + id, facade.makeOptions("PUT", true))
-            .then((response) => response.json())
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                return response.json();
+            })
             .then((data) => console.log(data))
             .catch((error) => console.log("An error occurred: ", error))
     }
 
     const deleteLentbook = (id) => {
         fetch(URL + "/lendbooks/" + id, facade.makeOptions("DELETE", true))
-            .then((response) => response.json())
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                return response.json();
+            })
             .then((data) => console.log(data))
             .catch((error) => console.log("An error occurred: ", error))
     }
